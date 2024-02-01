303e42227d404ac4426c17365e036d2c7d56bd85
Subha <154324356+subhacasual@users.noreply.github.com>
2024-02-01 11:39:24 +0530

    turn.py

---
+++ turn.py
@@ -0,0 +1,32 @@
+import os
+
+import streamlit as st
+from twilio.rest import Client
+
+
+@st.cache_data
+def get_ice_servers():
+    """Use Twilio's TURN server because Streamlit Community Cloud has changed
+    its infrastructure and WebRTC connection cannot be established without TURN server now.  # noqa: E501
+    We considered Open Relay Project (https://www.metered.ca/tools/openrelay/) too,
+    but it is not stable and hardly works as some people reported like https://github.com/aiortc/aiortc/issues/832#issuecomment-1482420656  # noqa: E501
+    See https://github.com/whitphx/streamlit-webrtc/issues/1213
+    """
+
+    # Ref: https://www.twilio.com/docs/stun-turn/api
+    try:
+        account_sid = os.environ["AC90c93b83ff1a1634c360fff5c213aa77"]
+        auth_token = os.environ["834cc8920bb96f9e9fff550513737cbf"]
+      print("Twilo Authenticated Successful")
+    except KeyError:
+        print("Twilio credentials are not set. Fallback to a free STUN server from Google.")
+        # logger.warning(
+        #     "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
+        # )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    client = Client(account_sid, auth_token)
+
+    token = client.tokens.create()
+
+    return token.ice_servers
