1b1c23753f62d532d691ee06e5d31625249e530a
Subha <154324356+subhacasual@users.noreply.github.com>
2024-02-01 11:40:54 +0530

    Update yolo_app.py

--- yolo_app.py
+++ yolo_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer, WebRtcMode
 import av
 from yolo_com_od import detect_objects
-
+from turn import get_ice_servers
 
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     image = frame.to_ndarray(format="bgr24")
@@ -13,9 +13,18 @@ def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     return av.VideoFrame.from_ndarray(result_frame, format="bgr24")
 
 
-webrtc_streamer(key="sample",
-                video_frame_callback=video_frame_callback,
-                mode=WebRtcMode.SENDRECV,
-                rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-                # media_stream_constraints={"video": True, "audio": False},
-                async_processing=True)
+# webrtc_streamer(key="sample",
+#                 video_frame_callback=video_frame_callback,
+#                 mode=WebRtcMode.SENDRECV,
+#                 rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+#                 # media_stream_constraints={"video": True, "audio": False},
+#                 async_processing=True)
+
+webrtc_streamer(
+    key="object-detection",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={"iceServers": get_ice_servers()},
+    video_frame_callback=video_frame_callback,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
